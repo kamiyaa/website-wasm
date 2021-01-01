64e1fee8689ca964e437f8960df3497b0d6a305c
Jiayi Zhao <jeff.no.zhao@gmail.com>
2021-01-01 10:11:28 -0500

    change icon sizes

--- src/components/social_links.rs
+++ src/components/social_links.rs
@@ -34,35 +34,35 @@ impl Component for SocialLinks {
             <li>
             <a
                 class="social-icon" target="_blank" rel="noopener noreferrer"
-                href={GITHUB_LINK}><i class="fab fa-github fa-2x"/>
+                href={GITHUB_LINK}><i class="fab fa-github fa-lg"/>
             </a>
             </li>
 
             <li>
             <a
                 class="social-icon" target="_blank" rel="noopener noreferrer"
-                href={LINKEDIN_LINK}><i class="fab fa-linkedin fa-2x"/>
+                href={LINKEDIN_LINK}><i class="fab fa-linkedin fa-lg"/>
             </a>
             </li>
 
             <li>
             <a
                 class="social-icon" target="_blank" rel="noopener noreferrer"
-                href={YOUTUBE_LINK}><i class="fab fa-youtube fa-2x"></i>
+                href={YOUTUBE_LINK}><i class="fab fa-youtube fa-lg"></i>
             </a>
             </li>
 
             <li>
             <a
                 class="social-icon"
-                href={EMAIL_LINK}><i class="fa fa-envelope fa-2x"/>
+                href={EMAIL_LINK}><i class="fa fa-envelope fa-lg"/>
             </a>
             </li>
 
             <li>
             <a
                 class="social-icon"
-                href={RESUME_LINK}><i class="fa fa-file fa-2x"/>
+                href={RESUME_LINK}><i class="fa fa-file fa-lg"/>
             </a>
             </li>
         </ul>
